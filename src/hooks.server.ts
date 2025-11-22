115d3d3d4412bedca60bc27e540ae820fc058b02
befabry <fabrybenjamin5@gmail.com>
2025-11-22 22:57:36 +0100

    refactor(hooks): convert arrow functions to named function declarations

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,24 +1,30 @@
 import type { Handle, HandleFetch } from '@sveltejs/kit';
 
-export const handle: Handle = async ({ event, resolve }) => {
+const allowedHeaders = ['content-type', 'content-length', 'content-encoding'];
+
+export async function handle({
+	event,
+	resolve
+}: Parameters<Handle>[0]): Promise<ReturnType<Handle>> {
 	const response = await resolve(event, {
-		filterSerializedResponseHeaders: (name) => name.startsWith('content')
+		filterSerializedResponseHeaders: (name) => allowedHeaders.includes(name.toLowerCase())
 	});
 
 	return response;
-};
+}
 
-export const handleFetch: HandleFetch = async ({ request, fetch }) => {
+const backend_url = process.env['VITE_BACKEND_URL']!;
+const frontend_url = `${process.env['VITE_FRONTEND_URL']}${process.env['VITE_API_BASE_URL']}}`;
+
+export async function handleFetch({
+	request,
+	fetch
+}: Parameters<HandleFetch>[0]): Promise<ReturnType<HandleFetch>> {
+	console.log(request.headers);
 	if (request.url.includes('/api/')) {
 		// clone the original request, but change the URL
-		request = new Request(
-			request.url.replace(
-				`${process.env['VITE_FRONTEND_URL']}${process.env['VITE_API_BASE_URL']}}`,
-				process.env['VITE_BACKEND_URL']!
-			),
-			request
-		);
+		request = new Request(request.url.replace(frontend_url, backend_url), request);
 	}
 
 	return fetch(request);
-};
+}
